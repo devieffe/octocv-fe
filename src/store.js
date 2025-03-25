c4657d73a6b02e0f30a6ec04fed2716c5d138d8f
pamellaester <you@example.com>
2025-03-25 18:46:41 +0200

    questionnaire update

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import cvReducer from "./slices/cvSlice";
+import questionnaireReducer from "./slices/questionnaireSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    questionnaire: questionnaireReducer,
     cv: cvReducer,
+
   },
 });
